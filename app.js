8402777b55a7471f5ff23b1f7c870ef8dcbae75a
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-06 09:50:00 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const shop = [
+    { name: 'Laptop asus', price: 10000, color: 'silver' },
+    { name: 'Laptop acer', price: 20000, color: 'silver' },
+    { name: 'Hp', price: 3000, color: 'silver' },
+    { name: 'Dell', price: 40000, color: 'silver' },
+    { name: 'Vivobok Laptop', price: 50000, color: 'silver' },
+]
